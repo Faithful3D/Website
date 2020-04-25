a6c55350232865497eada5209588fa387c5118df
Julien <Juknum@users.noreply.github.com>
2020-04-26 00:47:02 +0200

    Revert "Merge branch 'master' of https://github.com/Juknum/faithful3d.github.io"
    This reverts commit 9c18092ec56490e2ecce5db9183f43b52be39ead, reversing
    changes made to 460e5ee483865a4d227ff610051f551f477e76ca.

--- js/display.js
+++ js/display.js
@@ -36,12 +36,12 @@ const modalScene = new THREE.Scene();
 modalScene.rotation.y = Math.PI;
 
 //LIGHT
-const ambiLight = new THREE.AmbientLight(0xffffff, 0.8);
-ambiLight.position.set(0, 0, 0);
-imgScene.add(ambiLight);
-modalScene.add(ambiLight.clone());
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.5);
+hemiLight.position.set(0, 0, 0);
+imgScene.add(hemiLight);
+modalScene.add(hemiLight.clone());
 
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.2);
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
 dirLight.position.set(64, 64, -64);
 imgScene.add(dirLight);
 modalScene.add(dirLight.clone());
