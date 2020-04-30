f9626e41c93aeaa78ecd525b7b68911c95857354
Domi04151309 <domi04151309business@gmail.com>
2020-04-30 20:20:30 +0200

    Export download script

---
+++ js/download.js
@@ -0,0 +1,38 @@
+const ctx = document.getElementById('chart_download').getContext('2d');
+
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['November','December','January','Februar','March','April','May'],
+    datasets: [{
+      label: 'Total',
+      data: [414,4522,8527,14525,22753],
+      borderColor: 'rgb(255, 0, 0)',
+      backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      borderWidth: 1
+    },
+    {
+      label: 'Curse Forge',
+      data: [399,4341,7699,13041,20396],
+      borderColor: 'rgb(255, 128, 0)',
+      backgroundColor: 'rgba(255, 128, 0, 0.5)',
+      borderWidth: 1
+    },
+    {
+      label: 'Twitch',
+      data: [15,181,828,1484,2357],
+      borderColor: 'rgb(127, 0, 255)',
+      backgroundColor: 'rgba(127, 0, 255, 0.5)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
