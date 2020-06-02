21bac9d09b9f4f4d40ada657a1e9ae8d1e52deb0
Julien <Juknum@users.noreply.github.com>
2020-06-02 11:15:34 +0200

    change rgba color

--- js/download.js
+++ js/download.js
@@ -14,21 +14,21 @@ new Chart(document.getElementById('chart_download').getContext('2d'), {
       label: 'Total',
       data: [414,4108,4419,10106,12647,18850],
       borderColor: 'rgb(255, 0, 0)',
-      backgroundColor: 'rgba(255, 0, 0, 0.8)',
+      backgroundColor: 'rgba(255, 0, 0, 0.5)',
       borderWidth: 1
     },
     {
       label: 'Curse Forge',
       data: [399,3942,3757,9284,11112,17152],
       borderColor: 'rgb(255, 128, 0)',
-      backgroundColor: 'rgba(255, 128, 0, 0.8)',
+      backgroundColor: 'rgba(255, 128, 0, 0.5)',
       borderWidth: 1
     },
     {
       label: 'Twitch App',
       data: [15,166,662,822,1535,1698],
       borderColor: 'rgb(127, 0, 255)',
-      backgroundColor: 'rgba(127, 0, 255, 0.8)',
+      backgroundColor: 'rgba(127, 0, 255, 0.5)',
       borderWidth: 1
     }]
   },
