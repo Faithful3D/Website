96338d6a8083551a0d4f42a1a59b4a27743d68fa
Julien <Juknum@users.noreply.github.com>
2020-06-28 00:28:03 +0200

    change class to id

--- js/alert.js
+++ js/alert.js
@@ -2,8 +2,8 @@
 	Made from cookiebanner.js by Juknum
 */
 
-const alertContainer = document.querySelector(".alert-container");
-const alertBtn = document.querySelector(".alert-btn");
+const alertContainer = document.getElementById(".alert-container");
+const alertBtn = document.getElementById(".alert-btn");
 
 alertBtn.addEventListener("click", () => {
   alertContainer.classList.remove("active");
