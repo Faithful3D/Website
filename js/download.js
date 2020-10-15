87952231cd633e03c86c369dd31c61f278e7b98a
Julien <Juknum@users.noreply.github.com>
2020-10-15 23:12:24 +0200

    update download & remove curse/twitch app download (useless)

--- js/download.js
+++ js/download.js
@@ -4,7 +4,7 @@ if (dateArray) {
   dates = dateArray;
 } 
 else {
-  dates = ['11','12','1','2','3','4','5','6','7','8'];
+  dates = ['11','12','1','2','3','4','5','6','7','8','9','10'];
   console.log("No translation of months available. Falling back to numbers.");
 }
 
@@ -14,23 +14,9 @@ new Chart(document.getElementById('chart_download').getContext('2d'), {
     labels: dates,
     datasets: [{
       label: 'Total',
-      data: [ 414, 4522, 8527, 14525, 22753, 31497, 37941, 42629, 47796],
+      data: [ 414, 4522, 8527, 14525, 22753, 31497, 37941, 42629, 47796, 52202, 56194],
       borderColor: 'rgb(255, 0, 0)',
       backgroundColor: 'rgba(255, 0, 0, 0.5)',
-      borderWidth: 1
-    },
-    {
-      label: 'Curse Forge',
-      data: [ 399, 4341, 7699, 13041, 20396, 28264, 33884, 37920, 42298],
-      borderColor: 'rgb(255, 128, 0)',
-      backgroundColor: 'rgba(255, 128, 0, 0.5)',
-      borderWidth: 1
-    },
-    {
-      label: 'Twitch App',
-      data: [  15,  181,  828,  1484,  2357,  3233,  4057,  4709,  5498],
-      borderColor: 'rgb(127, 0, 255)',
-      backgroundColor: 'rgba(127, 0, 255, 0.5)',
       borderWidth: 1
     }]
   },
